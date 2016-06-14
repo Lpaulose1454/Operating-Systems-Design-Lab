a21ad195c16b442eaa418ff648c69c8ce16ceed8
Lpaulose1454 <lpaulose9@gmail.com>
2016-06-14 13:39:02 -0400

    Add files via upload

--- simpletask.c
+++ simpletask.c
@@ -1,20 +1,58 @@
+/*
+File         : simpletask.c
+Programmer   : Liyamoll Paulose - 7441454
+Description  : This file will reads the counter and
+               prints the message. The monitor command 
+               increments the counter
+Date         : 14/06/2016
+*/    
 #include <stdio.h>
 #include <stdint.h>
 #include "common.h"
 
-static uint32_t count;
-void TaskCounter(void)
+static uint32_t counter=0;    // initialise counter value
+
+/*
+Function     : TaskCounter() 
+Description  : checks whether the count value is equal
+               to zero and if not, print the count value 
+               and decrements after printing each value
+Parameters   : None
+Returns      : None
+*/
+void TaskCounter(void)   
 {
-while(count >0)
+if(counter!=0)               // checks whether counter value 
+                             // not equal to zero
 { 
- printf("Counting  is downwards: %u", count); 
-count--;
+ printf("The counter is %d\n",(unsigned int) count); 
+counter--;                   // decrements the counter
 }
 }
 
-void CmdCount()
+/*
+Function     : CmdCount() 
+Description  : Read the numeric argument and add number to 
+               current content of the global counter variable
+Parameters   : mode
+Returns      : None
+*/
+void CmdCount(int mode)
 {
-scanf(" Enter count: %u", count);
+int rc;
+uint32_t value = 0;         
+if(mode != CMD_INTERACTIVE)  // checks the mode value
+ {
+    return;
+ }
+rc = fetch_uint32_arg(&value);// read the numeric argument
+if(rc)
+ {
+    printf(" Enter value");
+    return;
+ }
+else
+counter=value;                // add number to the counter value
   }
+ADD_CMD("count",CmdCount," Counting downwards");
 
-ADD_CMD("count", CmdCount,"Reads the counter");
